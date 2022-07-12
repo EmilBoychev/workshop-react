3e81efb265c1e89922f0139127af1b25c7d0856c
Emil Boychev <85123165+EmilBoychev@users.noreply.github.com>
2022-07-12 15:27:13 +0300

    Add Create , Register  and Login components  in App component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import { About } from "./components/About/About";
 import { Banner } from "./components/Banner/Banner";
 import { ClientSection } from "./components/ClientSection/ClientSection";
 import { Contacts } from "./components/Contacts/Contacts";
+import { Create } from "./components/Create/Create";
 import { Footer } from "./components/Footer/Footer";
 import { Glasses } from "./components/Glasses/Glasses";
 import { Header } from "./components/Header/Header";
 import { Shop } from "./components/Shop/Shop";
+import { Login } from "./components/User/Register/Login";
+import { Register } from "./components/User/Register/Register";
 
 function App() {
   return (
@@ -24,6 +27,12 @@ function App() {
       <ClientSection />
 
       <Contacts />
+
+      <Create />
+
+      <Register />
+
+      <Login />
 
       <Footer />
     </div>
