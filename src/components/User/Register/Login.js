865b62349cf381f55890031dbe4f4f55930b012a
Emil Boychev <85123165+EmilBoychev@users.noreply.github.com>
2022-07-12 15:31:11 +0300

    Fix JSX in Login component

--- src/components/User/Register/Login.js
+++ src/components/User/Register/Login.js
@@ -8,16 +8,13 @@ export const Login = () => {
                         <form id="request" className="main_form">
                             <div className="row">
                                 <div className="col-md-12 ">
-                                    <h3>Register</h3>
+                                    <h3>Login</h3>
                                 </div>
                                 <div className="col-md-12">
                                     <input className="contactus" placeholder="User name" type="type" name="userName" />
                                 </div>
                                 <div className="col-md-12">
                                     <input className="contactus" placeholder="Password" type="type" name="password" />
-                                </div>
-                                <div className="col-md-12">
-                                    <input className="contactusmess" placeholder="Re-password" type="type" message="rePassword" />
                                 </div>
                                 <div className="col-md-12">
                                     <button className="send_btn">Send</button>
