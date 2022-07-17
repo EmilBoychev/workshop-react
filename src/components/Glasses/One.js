a973944f58d95d0a6b50a315a346083a95d858a8
Emil Boychev <85123165+EmilBoychev@users.noreply.github.com>
2022-07-17 22:55:02 +0300

    Rename Shop controller to Details

--- src/components/Glasses/One.js
+++ src/components/Glasses/One.js
@@ -1,10 +1,12 @@
-export const One = ({ glassesData }) => {
+export const One = ({ glassesData, clickDetails }) => {
+
+
     return (
-        <div className="col-xl-3 col-lg-3 col-md-6 col-sm-6">
+        <div className="col-xl-3 col-lg-3 col-md-6 col-sm-6" onClick={() => clickDetails(glassesData._id)} >
             <div className="glasses_box">
-                <figure><img src={glassesData.imgUrl} alt="#" /></figure>
-                <h3><span className="blu">$</span>{glassesData.price}</h3>
-                <p>{glassesData.name}</p>
+                <figure className="img-one" ><img src={glassesData.imgUrl} alt="#" /></figure>
+                <h3>{glassesData.name}</h3>
+                <p><span className="blu">$</span>{glassesData.price}</p>
             </div>
         </div>
     )
--- src/components/Shop/Details.js
+++ src/components/Shop/Details.js
@@ -1,24 +1,22 @@
+import { NavLink } from 'react-router-dom';
 import './style.css';
 
-export const Shop = () => {
+export const Details = ({ data }) => {
     return (
         <div id="about" className="shop">
             <div className="container-fluid">
                 <div className="row">
                     <div className="col-md-5">
                         <div className="shop_img">
-                            <figure><img src="images/shop_img.png" alt="#" /></figure>
+                            <figure><img src={data.imgUrl} alt="#" /></figure>
                         </div>
                     </div>
                     <div className="col-md-7 padding_right0">
                         <div className="max_width">
                             <div className="titlepage">
                                 <h2>Best SunGlasses At Our shop</h2>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                                    labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                                    laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-                                    voluptate velit esse cillum dolore</p>
-                                <a className="read_more" href="#">Shop Now</a>
+                                <p>{data.description}</p>
+                                <NavLink className="read_more" to="#">Buy now</NavLink>
                             </div>
                         </div>
                     </div>
