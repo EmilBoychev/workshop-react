eaf824b815efe53972ea0ad3d041e811029080cd
Emil Boychev <85123165+EmilBoychev@users.noreply.github.com>
2022-07-19 17:02:29 +0300

    Add dynamic render glasses on Banner component

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,7 +1,33 @@
-import { NavLink } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import * as GlassesService from '../Services/GlassesService'
+import { Glasses } from './Glasses/Glasses';
 import './style.css';
 
 export const Banner = () => {
+    const [glasses, setGlasses] = useState([]);
+    const [index, setIndex] = useState(0)
+    useEffect(() => {
+        GlassesService.getAll()
+            .then(glasses => {
+                setGlasses(glasses);
+            })
+    }, [])
+
+    const nextClickHandler = (e) => {
+        // e.preventDefult();
+
+        if (index !== glasses.length - 1) {
+            setIndex(state => state + 1);
+        } else {
+            setIndex(0);
+        }
+    }
+    const prevClickHandler = (e) => {
+        if (index <= 0) {
+            setIndex(glasses.length);
+        }
+        setIndex(state => state - 1);
+    }
     return (
         <section className="banner_main">
             <div id="banner1" className="carousel slide" data-ride="carousel">
@@ -11,46 +37,16 @@ export const Banner = () => {
                     <li data-target="#banner1" data-slide-to="2"></li>
                 </ol>
                 <div className="carousel-inner">
-                    <div className="carousel-item active">
-                        <div className="container">
-                            <div className="carousel-caption">
-                                <div className="text-bg">
-                                    <h1> <span className="blu">Welcome </span>To Our Sunglasses</h1>
-                                    <figure><img src="images/banner_img.png" alt="#" /></figure>
-                                    <NavLink className="read_more" to="/glasses">Shop Now</NavLink>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="carousel-item">
-                        <div className="container">
-                            <div className="carousel-caption">
-                                <div className="text-bg">
-                                    <h1> <span className="blu">Welcome </span>To Our Sunglasses</h1>
-                                    <figure><img src="images/banner_img.png" alt="#" /></figure>
-                                    <NavLink className="read_more" to="/glasses">Shop Now</NavLink>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="carousel-item">
-                        <div className="container">
-                            <div className="carousel-caption">
-                                <div className="text-bg">
-                                    <h1> <span className="blu">Welcome </span>To Our Sunglasses</h1>
-                                    <figure><img src="images/banner_img.png" alt="#" /></figure>
-                                    <NavLink className="read_more" to="/glasses">Shop Now</NavLink>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+
+                    <Glasses glasses={glasses[index]} />
+
                 </div>
-                {/* <NavLink className="carousel-control-prev" to="#banner1" role="button" data-slide="prev">
-                    <i className="fa fa-arrow-left" aria-hidden="true"></i>
-                </NavLink>
-                <NavLink className="carousel-control-next" to="#banner1" role="button" data-slide="next">
-                    <i className="fa fa-arrow-right" aria-hidden="true"></i>
-                </NavLink> */}
+                <div className="carousel-control-prev" role="button" data-slide="prev" onClick={prevClickHandler}>
+                    <i className="fa fa-arrow-left" aria-hidden="true" ></i>
+                </div>
+                <div className="carousel-control-next" role="button" data-slide="next" onClick={nextClickHandler}>
+                    <i className="fa fa-arrow-right" aria-hidden="true" ></i>
+                </div>
             </div>
         </section>
     )
---
+++ src/components/Banner/Glasses/Glasses.js
@@ -0,0 +1,18 @@
+import { NavLink } from "react-router-dom"
+
+export const Glasses = ({ glasses }) => {
+    return (
+        <div className="carousel-item active">
+            <div className="container">
+                <div className="carousel-caption">
+                    <div className="text-bg">
+                        <h1> <span className="blu">Welcome </span>To Our Sunglasses</h1>
+                        <figure><img className="banner-img" src={glasses?.imgUrl} alt="#" /></figure>
+                        <NavLink className="read_more" to={`/glasses/${glasses?._id}`}>Shop Now</NavLink>
+                    </div>
+                </div>
+            </div>
+        </div >
+
+    )
+}
